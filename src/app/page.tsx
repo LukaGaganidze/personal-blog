93bf58caaa5b3ed948fe75305fafac21f619d4df
Luka Gaganidze <LukaGaganidzeDev@gmail.com>
2023-09-16 18:13:26 +0400

    configured_for_deployment_2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import classes from "./Page.module.scss";
-
-// import { useContext } from "react";
-// import themeComtext from "../context/theme-context";
-
 import Hero from "../sections/home/hero/Hero";
 
 export default function Home() {
   return (
-    <div className={classes["home-page"]}>
+    <div>
       <Hero />
     </div>
   );
